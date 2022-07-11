133f0ad267ca25861231b24371184c649cbc5d2a
CuriousPlughole <curiousplughole@gmail.com>
2022-07-11 19:25:55 +0100

    Stuff got corrupted but hopefully its fixed now. Started adding Skaro plants. Need to make features with them for generation. Need to look into some way of ignoring item model jsons that have been manually added. For now just manually delete after runData.

--- src/main/java/com/prismmods/dwdimensions/handlers/ClientEvents.java
+++
@@ -1,9 +0,0 @@
-package com.prismmods.dwdimensions.handlers;
-
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.common.Mod;
-
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
-public class ClientEvents {
-
-}
